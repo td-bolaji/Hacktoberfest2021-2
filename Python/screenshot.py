5c94978f0cae97cc8cdb3deb005eef9d0d80a137
td-bolaji <60175519+td-bolaji@users.noreply.github.com>
2021-10-02 18:32:48 +0100

    Update screenshot.py

--- Python/screenshot.py
+++ Python/screenshot.py
@@ -1,3 +1,6 @@
 import pyautogui
+import random
+number = random.randint(0, 1000)
+number = str(number)
 screenshot = pyautogui.screenshot()
-screenshot.save("screenshot.png")                                                 
+screenshot.save(number + "screenshot.png")                                                 
